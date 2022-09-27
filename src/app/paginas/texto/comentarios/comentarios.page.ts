1c058ab0207486020ebbdbe408bf79c18814286f
rata-teck <shadowsmash5@gmail.com>
2022-09-27 14:30:17 -0300

    Libre de politica

--- src/app/paginas/texto/comentarios/comentarios.page.ts
+++ src/app/paginas/texto/comentarios/comentarios.page.ts
@@ -14,7 +14,6 @@ export class ComentariosPage implements OnInit {
 
   public comentario : Array<string> = [
     'Ya era hora que quitaran el uso obligatorio de mascarillas, dejé de usar lentes y llego to ahogao a casa con esa wea',
-    'Oe yapo merluzo ql, teni la pura caga en el país y queri seguir',
     'La sirenita original era demasiado linda para ser una sirena, esta negra es más a medida'
   ];
 
--- src/app/paginas/texto/noticias/noticias.page.ts
+++ src/app/paginas/texto/noticias/noticias.page.ts
@@ -15,7 +15,6 @@ export class NoticiasPage implements OnInit {
   public noticia : Array<string> = [
     'Wenaaaa! A partir del 1 de octubre, la mascarilla dejará de ser obligatoria',
     '"Wey ya!" Mexico sufre terremoto de magnitud 7.6, otra vez un 19 de septiembre',
-    'Pero wn! Gabriel Boric insulta a Carabineros y defiende el nombre de Allende ante la ONU',
     'Tengo miedo! La selección chilena se enfrentará a Marruecos el viernes a las 16 horas',
     'Panas! Empresas de videojuegos muestran su apoyo a Rockstar Games presentando sus proyectos en desarrollo'
   ];
